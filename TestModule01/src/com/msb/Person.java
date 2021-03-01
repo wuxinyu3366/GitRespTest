0dd3225fbd581c5bbba092bb01bd63645bf344be
wuxy <957904804@qq.com>
2021-03-01 20:36:21 +0800

    Person增加了age属性

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @author Administrator
+ * @Auther: wuxy
+ * @Date: 2021/3/1 - 03 - 01 - 20:34
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
